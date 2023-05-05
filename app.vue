f2ded005d3e6158dffb5ebd8d535f8dcb913b91d
lucas.dias <lucas.dias@integrativa.com.br>
2023-05-05 16:55:02 -0300

    fix: typo

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@
 					<div class="section one">
 						<DevIntro />
 					</div>
-					<div class="section two" style="height: 8000px">CU!</div>
+					<div class="section two" style="height: 8000px"></div>
 				</div>
 			</HijackedScroll>
 		</ClientOnly>
